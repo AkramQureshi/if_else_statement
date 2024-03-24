4457984584f3af0ca3202bccef1a93ed5e42345f
AkramQureshi <161283057+AkramQureshi@users.noreply.github.com>
2024-03-24 18:30:14 +0500

    Add files via upload

---
+++ if_else_statement.ts
@@ -0,0 +1,43 @@
+/*Syntax if(true){run this script} 
+else{}
+
+/*Syntax if(false){} Syntax 
+else{run this script}*/
+
+
+
+/*
+if(true){
+    console.log("Hello if")
+}  
+else{
+    console.log("Hello else")
+}
+
+if(false){
+    console.log("Hello if")
+}  
+else{
+    console.log("Hello else")
+}*/
+//condition True
+/*
+let user = "Akram Qureshi"
+
+if(user="Ashaz"){
+    console.log("Welcome User")
+}  
+else{
+    console.log("Ivalid User")
+}
+*/
+
+let user="akram qureshi"
+let pw="123"
+
+if(user=="akram qureshi" && pw=="123"){
+    console.log("welcome user")
+}
+else {
+    console.log("invalid user")
+}
